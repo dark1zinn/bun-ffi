368f87f0807efa7330d52c2908a2c6a03922a6da
dark1zinn <edilsonjuininho154@gmail.com>
2025-06-13 21:45:07 -0300

    🔨 Add typescript code to use Rust function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
+import { dlopen, FFIType } from "bun:ffi";
 import { definePlatformSuffix } from "./utils";
+import path from 'path';
 
 definePlatformSuffix()
+
+const libPath = path.join(
+    __dirname,
+    `../rust/target/release/rust.${process.env.PLATFORM_SUFFIX}`
+);
+
+const lib = dlopen(libPath, {
+    add: {
+        args: [FFIType.i32, FFIType.i32],
+        returns: FFIType.i32,
+    },  
+});
+
+const result = lib.symbols.add(3, 4);
+console.log(`Result from Rust: 3 + 4 = ${result}`);
